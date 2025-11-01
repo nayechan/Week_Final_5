076d2c61b61be1021aa18fe2a743442d084385ca
Sunha-i <icecream20@khu.ac.kr>
2025-11-01 22:52:11 +0900

    feat: add tile map actor

---
+++ Mundi/Source/Runtime/Engine/GameFramework/ATileMapActor.cpp
@@ -0,0 +1,74 @@
+﻿#include "pch.h"
+#include "ATileMapActor.h"
+#include "StaticMeshComponent.h"
+#include "Source/Runtime/Core/Object/ObjectFactory.h"
+
+IMPLEMENT_CLASS(ATileMapActor)
+
+BEGIN_PROPERTIES(ATileMapActor)
+	MARK_AS_SPAWNABLE("타일맵", "타일 기반의 맵을 생성하는 액터입니다.");
+END_PROPERTIES()
+
+ATileMapActor::ATileMapActor()
+	: MapWidth(10)
+	, MapHeight(10)
+	, TileSize(20.0f)
+{
+	GenerateGrid();
+}
+
+void ATileMapActor::GenerateGrid()
+{
+	for (UStaticMeshComponent* Tile : Tiles)
+	{
+		if (Tile)
+		{
+			Tile->DestroyComponent();
+		}
+	}
+	Tiles.clear();
+	Tiles.reserve(MapWidth * MapHeight);
+
+	FString MeshPath = GDataDir + "/Model/Cube.obj";
+	UStaticMesh* TileMesh = UResourceManager::GetInstance().Load<UStaticMesh>(MeshPath);
+	
+	const FAABB& MeshBounds = TileMesh->GetLocalBound();
+	TileSize = MeshBounds.Max - MeshBounds.Min;
+
+	for (int32 Y = 0; Y < MapHeight; Y++)
+	{
+		for (int32 X = 0; X < MapWidth; X++)
+		{
+			UStaticMeshComponent* TileComp = CreateDefaultSubobject<UStaticMeshComponent>("TileComponent");
+			TileComp->SetRelativeLocation(FVector(X * TileSize.X, Y * TileSize.Y, 0.0f));
+			TileComp->SetStaticMesh(GDataDir + "/Model/Cube.obj");
+			TileComp->SetupAttachment(RootComponent);
+			Tiles.push_back(TileComp);
+		}
+	}
+}
+
+bool ATileMapActor::WorldToTile(const FVector& WorldPos, int32& OutX, int32& OutY) const
+{
+	const FTransform& ActorTransform = GetActorTransform();
+	FTransform InverseTransform = ActorTransform.Inverse();
+	FVector LocalPos = InverseTransform.TransformPosition(WorldPos);
+
+	OutX = static_cast<int32>(floor(LocalPos.X / TileSize.X));
+	OutY = static_cast<int32>(floor(LocalPos.X / TileSize.Y));
+
+	return (OutX >= 0 && OutX < MapWidth && OutY >= 0 && OutY < MapHeight);
+}
+
+void ATileMapActor::DestroyTileAt(int32 X, int32 Y)
+{
+	if (X >= 0 && X < MapWidth && Y >= 0 && Y < MapHeight)
+	{
+		int32 Index = Y * MapWidth + X;
+		if (Index < Tiles.size() && Tiles[Index] != nullptr)
+		{
+			Tiles[Index]->DestroyComponent();
+			Tiles[Index] = nullptr;
+		}
+	}
+}
---
+++ Mundi/Source/Runtime/Engine/GameFramework/ATileMapActor.h
@@ -0,0 +1,22 @@
+﻿#pragma once
+#include "StaticMeshActor.h"
+
+class ATileMapActor : public AStaticMeshActor
+{
+public:
+	DECLARE_CLASS(ATileMapActor, AStaticMeshActor)
+	GENERATED_REFLECTION_BODY()
+
+	ATileMapActor();
+
+	void GenerateGrid();
+	bool WorldToTile(const FVector& WorldPos, int32& OutX, int32& OutY) const;
+	void DestroyTileAt(int32 X, int32 Y);
+
+private:
+	TArray<UStaticMeshComponent*> Tiles;
+
+	int32 MapWidth;
+	int32 MapHeight;
+	FVector TileSize;
+};
