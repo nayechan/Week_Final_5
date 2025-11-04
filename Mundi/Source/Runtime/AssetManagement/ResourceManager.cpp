2da75ad89e6928df38fff88e0b7f8d7762a32297
fuenell <wlscjf1404@gmail.com>
2025-11-05 07:48:29 +0900

    fix: 안 켜지는 현상 해결

--- Mundi/Source/Runtime/AssetManagement/ResourceManager.cpp
+++ Mundi/Source/Runtime/AssetManagement/ResourceManager.cpp
@@ -449,7 +449,7 @@ void UResourceManager::InitShaderILMap()
     TArray<D3D11_INPUT_ELEMENT_DESC> layout;
 
     layout.Add({ "POSITION", 0, DXGI_FORMAT_R32G32B32_FLOAT, 0, 0, D3D11_INPUT_PER_VERTEX_DATA, 0 });
-    //layout.Add({ "NORMAL", 0, DXGI_FORMAT_R32G32B32_FLOAT, 0, 12, D3D11_INPUT_PER_VERTEX_DATA, 0 });
+    layout.Add({ "NORMAL", 0, DXGI_FORMAT_R32G32B32_FLOAT, 0, 12, D3D11_INPUT_PER_VERTEX_DATA, 0 });
     ShaderToInputLayoutMap["Shaders/UI/Gizmo.hlsl"] = layout;
 	layout.clear();
 
---
+++ Mundi/Source/Runtime/Engine/GameFramework/PlayerCameraManager.h
@@ -0,0 +1,26 @@
+﻿//#pragma once
+//#include "Actor.h"
+//
+//class APlayerCameraManager : public AActor
+//{
+//public:
+//	DECLARE_CLASS(APlayerCameraManager, AActor)
+//	GENERATED_REFLECTION_BODY()
+//
+//	APlayerCameraManager();
+//
+//	
+//	// 선택: 스무딩/보간 캐시가 있다면 여기서 동기화
+//	void SyncRotationCache();
+//
+//	void SetPerspectiveCameraInput(bool InPerspectiveCameraInput);
+//
+//protected:
+//	~APlayerCameraManager() override;
+//
+//public:
+//
+//	DECLARE_DUPLICATE(APlayerCameraManager)
+//private:
+//};
+//
